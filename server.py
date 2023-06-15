4f3ad1e2ea8491c8619358632625b5e120ddc69d
Kyle Benson <kyleabenson@gmail.com>
2023-06-15 01:56:19 +0000

    testing with bq

--- server.py
+++ server.py
@@ -1,28 +1,32 @@
 from flask import Flask
 from google.cloud import bigquery
 
-
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    getAq()
     app.logger.info("Received request")
-    return 'Hello, World!'
+    try:
+        returnedArt = getArt()
+        return '<p> Behold! Art: %s </p>' % (returnedArt)
+    except IndexError:
+        abort(404)
+    
 
-def getAq():
+def getArt():
     client = bigquery.Client()
+    queryResults = []
     query_job = client.query(
         """
-        SELECT * FROM `bigquery-public-data.openaq.global_air_quality` WHERE country = "US" LIMIT 1"""
+        SELECT original_image_url FROM `bigquery-public-data.the_met.images` ORDER BY RAND ()  LIMIT 1"""
     )
-
-    results = query_job.result()  # Waits for job to complete.
-
-    for row in results:
-        app.logger.info(row)
-    return
+    try:
+        results = query_job.result()  # Waits for job to complete.
+        for rows in results:
+            queryResults.append(rows)
+        return queryResults[0]
+    except: 
+        print("No results, is big query... down?")
 
 
 if __name__ == '__main__':
